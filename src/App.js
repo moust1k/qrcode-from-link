f8346822ab3df296ce61819d14946b8032edd652
Sergey <methodman41@gmail.com>
2023-01-19 13:16:09 +0300

    QRCode App
    application for generate QRCode from link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import QRCode from 'react-qr-code'
+import './app.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [input, setInput] = useState('')
+	const [inputValue, setInputValue] = useState('')
+
+	const handleGenerate = () => {
+		if (input.trim()) {
+			setInputValue(input)
+			setInput('')
+		}
+	}
+
+	return (
+		<>
+			<div className='app'>
+				<div className='app__container'>
+					<h1 className='app__title'>Generate QRcode from link</h1>
+					<div className='app__form'>
+						<input type='text' placeholder='https://www.google.com' value={input} onChange={e => setInput(e.target.value)} />
+						<button onClick={handleGenerate}>Generate QR</button>
+					</div>
+					<QRCode bgColor='rgba(241, 227, 227, 0.596)' style={{ height: 'auto', width: '300px' }} value={inputValue} />
+				</div>
+			</div>
+		</>
+	)
 }
 
-export default App;
+export default App
